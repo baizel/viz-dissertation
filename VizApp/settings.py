f873c86b523ada4b8b82e71e9170de1b554f59e6
Baizel <baizelmathew@yahoo.co.in>
2019-01-30 16:51:04 +0000

    Minor change

--- VizApp/settings.py
+++ VizApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-#jYeqXx0TUoK7U2vc
+#XypWofo1JMJo0kVo
 
 import os
 
